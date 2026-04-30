48acb80288084a14245d5b4bf90b45e21e7925e8
khansa-qonita <khansaqonita420@gmail.com>
2026-04-30 12:10:41 +0700

    menambahkan fungsi hapus dan pengecekan list kosong

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -81,6 +81,23 @@ public :
 
         current->next = newNode;  //step 9d: current.next = newNode
     }
+
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted : ";
+        int rollNo;
+        cin >> rollNo;
+
+        Node *current = START;
+
+        
+    }
     
 
 }
