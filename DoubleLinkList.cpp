f56870bb46b308b1b77f7de081318c5b5f8ba166
khansa-qonita <khansaqonita420@gmail.com>
2026-04-30 11:54:10 +0700

    mengatur pointer prev pada START untuk koneksi dua arah

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -44,6 +44,10 @@ public :
             }
         //step 4: newNode.next = START
         newNode->next = START;
+
+        //step 5: START.prev = newNode (if START exists)
+        if (START != NULL)
+            START->prev = newNode;
         } 
 
 
