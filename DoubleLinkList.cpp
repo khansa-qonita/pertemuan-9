fbf4f91830e1bc82b4bd05751d6bf4a46a8fc22b
khansa-qonita <khansaqonita420@gmail.com>
2026-04-30 12:20:32 +0700

    menghubungkan pointer next dari node sebelum current ke depan

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -115,6 +115,8 @@ public :
         }
         else
         {
+           //step 3: link previous node to next of current 
+           current->prev->next = current->next;
            
         }
 
