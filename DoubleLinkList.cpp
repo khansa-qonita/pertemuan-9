9078451187fb4144ab8aa25390224e15725e4a57
khansa-qonita <khansaqonita420@gmail.com>
2026-04-30 12:19:07 +0700

    implementasi logika penghapusan node di awal list

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -106,6 +106,18 @@ public :
             return;
         }
 
+        //step 2: if node is at the beginning 
+        if (current == START)
+        {
+            START = current->next;   //step 2a: START = START.next
+            if (START !=  NULL)
+                START->prev = NULL;   //step 2b: START.prev = NULL        
+        }
+        else
+        {
+           
+        }
+
 
     }
     
