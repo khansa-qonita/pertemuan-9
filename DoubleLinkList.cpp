7945d6000bb0543b07d558d199ae516ce7020065
khansa-qonita <khansaqonita420@gmail.com>
2026-04-30 12:01:42 +0700

    implementasi pencarian posisi penyisipan dan validasi duplikat

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -42,20 +42,34 @@ public :
                 cout << "\nDuplicate number not allowed" << endl;
                 return;
             }
-        //step 4: newNode.next = START
-        newNode->next = START;
+            //step 4: newNode.next = START
+            newNode->next = START;
 
-        //step 5: START.prev = newNode (if START exists)
-        if (START != NULL)
-            START->prev = newNode;
+            //step 5: START.prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev = newNode;
 
-        //step 6: newNode.prev = NULL
-        newNode->prev = NULL;
+            //step 6: newNode.prev = NULL
+            newNode->prev = NULL;
 
-        //step 7: START =newNode
-        START =newNode;
-        return;
+            //step 7: START =newNode
+            START =newNode;
+            return;
         } 
+
+        //insert in between node
+        //step 8: locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
 
 
 
