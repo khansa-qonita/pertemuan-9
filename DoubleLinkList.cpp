69900b8aea4bab1b02cbf329b08b46fbae116e9e
khansa-qonita <khansaqonita420@gmail.com>
2026-04-30 11:44:49 +0700

    implementasi langkah pertama pembuatan node baru

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -28,6 +28,11 @@ public :
         cout << "\nEnter the roll number of the student : ";
         cin >> nim;
 
+        //step 1: allocate memory for new node
+        Node *newNode = new Node();
+
+        
+
     
     }
     
